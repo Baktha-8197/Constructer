ba651f2a86cc898ad27e372ac616864f4efcec22
Baktha-8197 <kbaktha8197@gmail.com>
2021-09-03 22:09:20 +0530

    success

--- src/org/test/Client.java
+++ src/org/test/Client.java
@@ -5,6 +5,14 @@ public Client() {
 	this("Mahavishnu");
 System.out.println("Client id is 5644");
 }
+
+private void method1() {
+System.out.println("sappadu");
+}
+
+private void method2() {
+System.out.println("Dancing rose");
+}
 public Client(String clientName) {
 	this(564487262);
 	System.out.println("Client Name is "+clientName);
